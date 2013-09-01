5ac8adaeffdca2194fb8e271bab299ec205eb0d0
Tyler Treat <ttreat31@gmail.com>
2013-09-01 17:28:23 -0500

    Add API endpoint for kicking off aggregation process

--- ripl/core/api/views.py
+++ ripl/core/api/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 
 from ripl.core.api.blueprint import blueprint
+from ripl.core.aggregator import aggregate
+from ripl.core.decorators import admin_required
 
 
 # Error handlers
@@ -11,5 +13,17 @@ def page_not_found(e):
 
 @blueprint.route('/')
 def index():
+    """Render the index page."""
+
     return render_template('index.html')
+
+
+@blueprint.route('/aggregate')
+@admin_required
+def aggregate_trends():
+    """Kick off the trend aggregation process. This is intended to be called by
+    a cron job.
+    """
+
+    aggregate()
 
