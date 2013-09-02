2b0643dfd51822bd15a29a074dc9ce433e39b310
Tyler Treat <ttreat31@gmail.com>
2013-09-02 03:09:14 -0500

    Implement Location and Trend entities

--- ripl/core/aggregation/__init__.py
+++ ripl/core/aggregation/__init__.py
@@ -16,3 +16,35 @@ class ApiToken(ndb.Model):
     def _pre_put_hook(self):
         self.last_modified = datetime.now()
 
+
+class Location(ndb.Model):
+    """Models a geographic location."""
+
+    # The "user-friendly" location name
+    name = ndb.StringProperty(required=True)
+
+    # The "user-friendly" location type (e.g. town, country, etc.)
+    type_name = ndb.StringProperty(indexed=False)
+
+    # Location type code
+    type_code = ndb.IntegerProperty(indexed=False)
+
+    # The WOEID of this Location's parent
+    parent_id = ndb.IntegerProperty(indexed=False)
+
+    # The "user-friendly" name of the country this Location is in
+    country = ndb.StringProperty()
+
+    # The code for the country this Location is in
+    country_code = ndb.StringProperty(indexed=False)
+
+
+class Trend(ndb.Model):
+    """Models a trending topic at a given moment in time at a specific
+    location on Earth.
+    """
+
+    location = ndb.KeyProperty(kind=Location, required=True)
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+    name = ndb.StringProperty(required=True)
+
