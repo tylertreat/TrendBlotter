b2abded53959ae05bccd0645ef47816aa9b648f8
Tyler Treat <ttreat31@gmail.com>
2013-09-18 00:01:04 -0500

    Add unit tests for chunk

--- ripl/tests/core/aggregation/test_aggregator.py
+++ ripl/tests/core/aggregation/test_aggregator.py
@@ -207,3 +207,53 @@ class TestAggregateForLocations(unittest.TestCase):
         put_multi.assert_called_once_with(mock_locations)
         self.assertIsInstance(ctx.exception, Abort)
 
+
+class TestChunk(unittest.TestCase):
+
+    def test_empty_list(self):
+        """Ensure an empty list is returned when an empty list is passed."""
+        from ripl.core.aggregation.aggregator import chunk
+
+        self.assertEqual([], chunk([], 10).next())
+
+    def test_bad_chunk_size(self):
+        """Ensure an empty list is returned when a bad chunk size is passed."""
+        from ripl.core.aggregation.aggregator import chunk
+
+        self.assertEqual([], chunk([1, 2, 3, 4, 5], 0).next())
+
+    def test_chunking_equal_groups(self):
+        """Ensure the list is chunked properly into equal groups when it can be
+        evenly divided.
+        """
+        from ripl.core.aggregation.aggregator import chunk
+
+        the_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        chunk_size = 3
+        full = []
+
+        for group in chunk(the_list, chunk_size):
+            self.assertEqual(chunk_size, len(group))
+            full.extend(group)
+
+        self.assertEqual(the_list, full)
+
+    def test_chunking_equal_groups_but_one(self):
+        """Ensure the list is chunked properly into equal groups except for the
+        last when it cannot be evenly divided.
+        """
+        from ripl.core.aggregation.aggregator import chunk
+
+        the_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        chunk_size = 3
+        full = []
+
+        for i, group in enumerate(chunk(the_list, chunk_size)):
+            if i == 3:
+                self.assertEqual(1, len(group))
+            else:
+                self.assertEqual(chunk_size, len(group))
+            full.extend(group)
+
+        self.assertEqual(the_list, full)
+
